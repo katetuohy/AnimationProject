fe6cc21faa1f43f8882bd0fffcfec1e8aaa090d8
katetuohy <51720181+katetuohy@users.noreply.github.com>
2019-06-14 10:08:33 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/cs3500/animator/Excellence.java
    #	src/cs3500/animator/view/IView.java
    #	src/cs3500/animator/view/SVGAnimationVIew.java
    #	src/cs3500/animator/view/TextualAnimationView.java
    #	src/cs3500/animator/view/VisualAnimationView.java
    #	src/cs3500/controller/Controller.java
    #	src/cs3500/controller/IController.java

--- src/cs3500/animator/view/SVGAnimationView.java
+++ src/cs3500/animator/view/SVGAnimationView.java
@@ -1,39 +1,22 @@
 package cs3500.animator.view;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import cs3500.model.AShape;
-import cs3500.model.Command;
-import cs3500.model.Shape;
 
 public class SVGAnimationView implements IView {
-  Appendable out;
-  int speed;
-
   @Override
-  public void displayTextualView(LinkedHashMap<Command, Shape> commands) {
-    throw new UnsupportedOperationException("Only for the textual view.");
-  }
-
-  @Override
-  public void display(List<Shape> shapes) {
+  public void display(List<AShape> Shapes) {
 
   }
 
   @Override
-  public void setShapesAndMotions(List<Shape> shapes, List<Command> motions) {
+  public void setOutput(String output) {
 
-  }
-
-  @Override
-  public void setOutput(Appendable output) {
-    this.out = output;
   }
 
   @Override
   public void setSpeed(int num) {
-    this.speed = speed;
+
   }
 }
