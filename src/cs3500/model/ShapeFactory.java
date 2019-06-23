1af6480af19f8396aac262c88bd144b5dd6f7b6c
Rebecca Mendez <35311987+rcmendez23@users.noreply.github.com>
2019-06-22 22:33:05 -0400

    Broke everything!

--- src/cs3500/model/ShapeFactory.java
+++ src/cs3500/model/ShapeFactory.java
@@ -18,10 +18,10 @@ public class ShapeFactory {
     if (type == null) { //maybe throw null exception here
       return null;
     }
-    if (type.equalsIgnoreCase("rectangle")) {
+    if (type.equalsIgnoreCase("rectangle") || type.equalsIgnoreCase("rect")) {
       return new Polygon(name, 4);
     }
-    if (type.equalsIgnoreCase("ellipse")) {
+    if (type.equalsIgnoreCase("ellipse") || type.equalsIgnoreCase("oval")) {
       return new Oval(name);
     }
     return null;
@@ -32,10 +32,10 @@ public class ShapeFactory {
     if (type == null) {
       return null;
     }
-    if (type.equalsIgnoreCase("rectangle")) {
+    if (type.equalsIgnoreCase("rectangle") || type.equalsIgnoreCase("rect")) {
       return new Polygon(name, 4, c, width, height, pos, true);
     }
-    if (type.equalsIgnoreCase("ellipse")) {
+    if (type.equalsIgnoreCase("ellipse") || type.equalsIgnoreCase("oval")) {
       return new Oval(name, c, width, height, pos, true);
     }
     return null;
